9a911664efbb5e44d57057f25317c6778d890ac4
Minty <dylan.poe1998@gmail.com>
2023-05-10 07:35:00 -0400

    Reads inputs and prints them.

--- AlignmentDashboard.py
+++ AlignmentDashboard.py
@@ -6,65 +6,65 @@ inputBoxWidth = 10
 
 Left_Wheels_Column = [
     [
-        gui.Text('Front Left Front', justification = 'right', expand_x = True, pad = ((5,5),(115,5))), gui.InputText(size = inputBoxWidth, pad = ((5,5),(115,5)))
+        gui.Text('Front Left Front', justification='right', expand_x=True, pad=((5,5),(115,5))), gui.InputText(size=inputBoxWidth, pad=((5,5),(115,5)), enable_events=True)
     ],
     [
-        gui.Text('Front Left Rear', justification = 'right', expand_x = True, pad = ((5,5),(1,75))), gui.InputText(size = inputBoxWidth, pad = ((5,5),(1,75)))
+        gui.Text('Front Left Rear', justification='right', expand_x=True, pad=((5,5),(1,75))), gui.InputText(size=inputBoxWidth, pad=((5,5),(1,75)), enable_events=True)
     ],
     [
-        gui.Text('Rear Left Front', justification = 'right', expand_x = True), gui.InputText(size = inputBoxWidth)
+        gui.Text('Rear Left Front', justification='right', expand_x=True), gui.InputText(size=inputBoxWidth, enable_events=True)
     ],
     [
-        gui.Text('Rear Left Rear', justification = 'right', expand_x = True), gui.InputText(size = inputBoxWidth)
+        gui.Text('Rear Left Rear', justification='right', expand_x=True), gui.InputText(size=inputBoxWidth, enable_events=True)
     ],
 ]
 
 Right_Wheels_Column = [
     [
-        gui.InputText(size = inputBoxWidth, pad = ((5,5),(115,5))), gui.Text('Front Right Front', justification = 'right', expand_x = True, pad = ((5,5),(115,5)))
+        gui.InputText(size=inputBoxWidth, pad=((5,5),(115,5)), enable_events=True), gui.Text('Front Right Front', justification='right', expand_x=True, pad=((5,5),(115,5)))
     ],
     [
-        gui.InputText(size = inputBoxWidth, pad = ((5,5),(1,75))), gui.Text('Front Right Rear', justification = 'right', expand_x = True, pad = ((5,5),(1,75)))
+        gui.InputText(size=inputBoxWidth, pad=((5,5),(1,75)), enable_events=True), gui.Text('Front Right Rear', justification='right', expand_x=True, pad=((5,5),(1,75)))
     ],
     [
-        gui.InputText(size = inputBoxWidth), gui.Text('Rear Right Front', justification = 'right', expand_x = True)
+        gui.InputText(size=inputBoxWidth, enable_events=True), gui.Text('Rear Right Front', justification='right', expand_x=True)
     ],
     [
-         gui.InputText(size = inputBoxWidth), gui.Text('Rear Right Rear', justification = 'right', expand_x = True)
+         gui.InputText(size=inputBoxWidth, enable_events=True), gui.Text('Rear Right Rear', justification='right', expand_x=True)
     ],
 ]
 
 Center_Data_Column = [
     [
-        gui.Text('Wheel Diameter', justification = 'center', expand_x = True)
+        gui.Text('Wheel Diameter', justification='center', expand_x=True)
     ],
     [
-        gui.InputText(size = inputBoxWidth, pad = (100,0))
+        gui.InputText(size=inputBoxWidth, pad=(100,0), enable_events=True)
     ],
     [
-        gui.Text('****Positive Toe is Tow In****', justification = 'center', expand_x = True, pad = ((5,5),(30,30)))
+        gui.Text('****Positive Toe is Tow In****', justification='center', expand_x=True, pad=((5,5),(30,30)))
     ],
     [
-        gui.Text('Left Front Toe: ', justification = 'center', expand_x = True)
+        gui.Text('Left Front Toe: ', justification='center', expand_x=True)
     ],
     [
-        gui.Text('Right Front Toe: ', justification = 'center', expand_x = True)
+        gui.Text('Right Front Toe: ', justification='center', expand_x=True)
     ],
     [
-        gui.Text('Total Front Toe: ', justification = 'center', expand_x = True, pad = ((5,5),(5,50)))
+        gui.Text('Total Front Toe: ', justification='center', expand_x=True, pad=((5,5),(5,50)))
     ],
     [
-        gui.Text('Left Rear Toe: ', justification = 'center', expand_x = True)
+        gui.Text('Left Rear Toe: ', justification='center', expand_x=True)
     ],
     [
-        gui.Text('Right Rear Toe: ', justification = 'center', expand_x = True)
+        gui.Text('Right Rear Toe: ', justification='center', expand_x=True)
     ],
     [
-        gui.Text('Total Rear Toe: ', justification = 'center', expand_x = True)
+        gui.Text('Total Rear Toe: ', justification='center', expand_x=True)
     ]
 ]
 
-layout = [
+layout=[
     [
         gui.Column(Left_Wheels_Column),
         gui.Column(Center_Data_Column),
@@ -72,17 +72,14 @@ layout = [
     ]
 ]
 
-defaultlayout = [  [gui.Text('Test')],
-            [gui.Text('Another Test. Give me somthing to say back'), gui.InputText()],
-            [gui.Button('Ok'), gui.Button('Cancel')] ]
-
 # Create the Window
-window = gui.Window('String Alignment Assistant', layout)
+window=gui.Window('String Alignment Assistant', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == gui.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event in (gui.WIN_CLOSED, 'Cancel'): # if user closes window or clicks cancel
         break
-    print('You entered ', values[0], '. Why are you such a smart ass?')
+    for x in range(len(values)):
+        print('You entered ', values[x], 'in text box', x+1)
 
 window.close()
